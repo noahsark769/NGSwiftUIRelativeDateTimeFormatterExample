a97a71e0f1a363e858981f174c1c08651dfacd62
Noah Gilmore <noah.w.gilmore@gmail.com>
2019-10-19 09:15:31 -0700

    Add example code for blog post

--- NGSwiftUIRelativeDateTimeFormatterExample/ContentView.swift
+++ NGSwiftUIRelativeDateTimeFormatterExample/ContentView.swift
@@ -8,11 +8,60 @@
 
 import SwiftUI
 
+// Code demonstrating the issue:
 struct ContentView: View {
+    static let formatter = RelativeDateTimeFormatter()
+
     var body: some View {
-        Text("Hello, World!")
+        let unixEpoch = Date(timeIntervalSince1970: 0)
+        let components = Calendar.current.dateComponents(
+            [.day, .year, .month, .minute, .second],
+            from: Date(),
+            to: unixEpoch
+        )
+        return VStack {
+            Text("Current date is:")
+            Text("\(components, formatter: Self.formatter)").bold()
+            Text("since the unix Epoch")
+            Spacer()
+        }
     }
 }
+
+// Solution 1
+//struct ContentView: View {
+//    static let formatter = RelativeDateTimeFormatter()
+//
+//    var body: some View {
+//        let unixEpoch = Date(timeIntervalSince1970: 0)
+//        return VStack {
+//            Text("Current date is:")
+//            Text("\(unixEpoch, formatter: Self.formatter)").bold()
+//            Text("since the unix Epoch")
+//            Spacer()
+//        }
+//    }
+//}
+
+// Solution 2
+//struct ContentView: View {
+//    static let formatter = RelativeDateTimeFormatter()
+//
+//    var body: some View {
+//        let unixEpoch = Date(timeIntervalSince1970: 0)
+//        let components = Calendar.current.dateComponents(
+//            [.day, .year, .month, .minute, .second],
+//            from: Date(),
+//            to: unixEpoch
+//        )
+//        return VStack {
+//            Text("Current date is:")
+//            Text("\(Self.formatter.localizedString(from: components))").bold()
+//            Text("since the unix Epoch")
+//            Spacer()
+//        }
+//    }
+//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
