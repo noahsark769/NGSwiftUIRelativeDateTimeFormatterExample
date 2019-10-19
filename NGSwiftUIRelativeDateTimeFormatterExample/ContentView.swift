8a5acf61e12f1b8185a210034215c4f89963e424
Noah Gilmore <noah.w.gilmore@gmail.com>
2019-10-19 08:44:46 -0700

    Initial Commit

---
+++ NGSwiftUIRelativeDateTimeFormatterExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NGSwiftUIRelativeDateTimeFormatterExample
+//
+//  Created by Noah Gilmore on 10/19/19.
+//  Copyright © 2019 Noah Gilmore. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
